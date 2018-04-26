a7dd69872859f1c11adedae0cb4f8c6c5c417076
blevm <mjblevins7@gmail.com>
2018-04-26 14:49:35 -0400

    solution

---
+++ lib/run.rb
@@ -0,0 +1,9 @@
+require_relative 'transfer'
+require_relative 'bank_account'
+require 'pry'
+
+# louis_account = BankAccount.new("Louis")
+# first_transfer = Transfer.new(, "Michelle", 50)
+
+
+Pry.start
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,47 @@
+require_relative 'bank_account'
+require 'pry'
+
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  TRANSFERS = []
+
+  def initialize(sender, receiver, amount, status = "pending")
+    #@sender = BankAccount.all.find {|account| account == sender}
+    @sender = sender
+    #@receiver = BankAccount.all.find {|account| account == receiver}
+    @receiver = receiver
+    @status = status
+    @amount = amount
+    TRANSFERS << self
+  end
+
+  def self.all
+    TRANSFERS
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if sender.valid? && receiver.valid? && self.status != "complete" && sender.balance >= amount
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance += self.amount
+      receiver.balance -= self.amount
+      self.status = "reversed"
+    end
+  end
+
 end
