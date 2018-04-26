78e50ab930d306c4464a02bb5e1b3113ec263eaa
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-26 19:36:09 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
 class BankAccount
 
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    def initialize(name)
+      @name = name
+      @balance = 1000
+      @status = "open"
+    end
+
+
+    #use method= when setting
+    #in this case we use attr_accessor to set only using below
+    #method to call
+
+    def deposit(amount)
+      @balance += amount
+    end
+
+    def pay_out(amount)
+      @balance -= amount
+    end
+
+    def display_balance
+       "Your balance is $#{@balance}."
+    end
+
+    def valid?
+      return true if @balance > 0 && @status == "open"
+      false
+    end
+
+    def close_account
+      @status = "closed"
+    end
+
 end
