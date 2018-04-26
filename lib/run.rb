2a66a5df6feb50b39cb4c731afb432f1c4b7d62d
blevm <mjblevins7@gmail.com>
2018-04-26 14:52:39 -0400

    updated solution

--- lib/run.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'transfer'
-require_relative 'bank_account'
-require 'pry'
-
-# louis_account = BankAccount.new("Louis")
-# first_transfer = Transfer.new(, "Michelle", 50)
-
-
-Pry.start
